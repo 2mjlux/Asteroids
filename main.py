94ef5ae20a23f30a45cd8998f326865f8c84252a
Micha <177941509+2mjlux@users.noreply.github.com>
2025-08-28 22:08:13 +0200

    B: Add CircleShape class

--- main.py
+++ main.py
@@ -3,6 +3,27 @@
 # throughout this file
 import pygame
 
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
+
 from constants import *
 
 print("Starting Asteroids!")
@@ -13,10 +34,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Hello from asteroids!")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            else:
+                dt = clock.tick(60)/1000
         screen.fill("black")
         pygame.display.flip()
         
