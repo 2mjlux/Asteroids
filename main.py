be7e8575a431ba29fe63934719c67f8755ef1526
Micha <177941509+2mjlux@users.noreply.github.com>
2025-09-03 11:22:19 +0200

    D: Added asteroid, asteroidfield and shot

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 print("Starting Asteroids!")
 print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,15 +19,30 @@ def main():
     print("Hello from asteroids!")
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable,)
+    new_asteroidfield = AsteroidField()
+    Shot.containers = (updatable, drawable, shots)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             else:
+                updatable.update(dt)
+                for asteroid in asteroids:
+                    if asteroid.check_collision(player):
+                        sys.exit("Game over!")
+                screen.fill("black")
+                for sprite in drawable:
+                    sprite.draw(screen)
+                pygame.display.flip()
                 dt = clock.tick(60)/1000
-        screen.fill("black")
-        pygame.display.flip()
         
-
 if __name__ == "__main__":
     main()
